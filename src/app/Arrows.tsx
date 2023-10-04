4262e88b4a17c6e08a2c4d1c2f5ed0b047373401
vlados2108 <verlov.vladislav@mail.ru>
2023-10-04 15:02:38 +0300

    added illumination to selected arrow

--- src/app/Arrows.tsx
+++ src/app/Arrows.tsx
@@ -1,25 +1,35 @@
 'use client'
-import React, { ReactElement } from 'react'
+import React, { ReactElement, useState } from 'react'
 import styles from './Arrows.module.scss'
 interface IArrowProps {
     sortAsc: () => void
     sortDsc: () => void
 }
 const Arrows = ({ sortAsc, sortDsc }: IArrowProps): ReactElement => {
+    const [isActiveup, setIsActiveUp] = useState(false)
+    const [isActiveDown, setIsActiveDown] = useState(false)
     return (
         <div className={styles['filter-image-container']}>
             <img
                 src="/arrowup.png"
-                className={`${styles['filter-image']} ${styles['up']}`}
+                className={`${styles['filter-image']} ${
+                    isActiveup ? styles['activeup'] : ''
+                }`}
                 onClick={() => {
+                    setIsActiveUp(!isActiveup)
+                    setIsActiveDown(false)
                     sortAsc()
                 }}
                 loading="lazy"
             ></img>
             <img
                 src="/arrowdown.png"
-                className={`${styles['filter-image']} ${styles['down']}`}
+                className={`${styles['filter-image']} ${
+                    isActiveDown ? styles['activedown'] : ''
+                }`}
                 onClick={() => {
+                    setIsActiveDown(!isActiveDown)
+                    setIsActiveUp(false)
                     sortDsc()
                 }}
                 loading="lazy"
